00fcfb585cd0430b0cda7398123ff224b750139c
Adr1an3 <ady.developer@proton.me>
2023-09-03 06:04:49 +1000

    initial js setup

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+/* -- A simple fetch photo app using the Pixabay API -- */
+
+/* Select all initial interactive page elements */
+const inputSearch = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const myImgsBtn = document.getElementById("myImgsBtn");
+const dateEl = document.getElementById("date");
+const results = document.querySelector(".results");
+const errorMsgDisplay = document.querySelector(".errorDisplay");
+const errorMsgCloseBtn = document.getElementById("errorMsgCloseBtn");
